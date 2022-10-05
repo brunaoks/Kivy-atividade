44251f77151dc0fa60b8a2018f8a9ee42152fd3e
brunaoks <sfbruno2002@gmail.com>
2022-10-05 11:06:54 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+
+Window.clearcolor = (.5, .4, .6, 1)
+
+class LoginUI(BoxLayout):
+    pass
+
+
+class LoginApp(App):
+    def build(self):
+        return LoginUI()
+
+
+if __name__ == '__main__':
+    login = LoginApp()
+    Window.size=(397,697)
+    login.run()
